776e02e6b5226e599776aee5385012304c942292
Taufan Yunus <taufan.gorontalo@gmail.com>
2018-11-11 00:39:08 +0800

    Handling error Promise-Part1

---
+++ async_promise_all_with_handlingError.js
@@ -0,0 +1,18 @@
+// Model Error Handling Sederhana
+
+async function example6(){
+
+    let prom1 = Promise.resolve(10);
+    let prom2 = Promise.resolve(20);
+    let prom3 = new Promise((resolve,reject)=> {
+        resolve(30);
+    })
+
+    return Promise.all([prom1, prom2, prom3]).then(
+                ifTrue => { console.log(ifTrue) }, // akan di tampilkan semua data jika semua variabel promise resolve.
+                ifFalse => { console.log(ifFalse) } // jika ada data yang reject maka akan dijalankan ifFalse ini, 
+                );
+
+}
+
+example6();
